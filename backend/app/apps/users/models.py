a2bc58989867b9c90bf0bc80d74f29c121a38943
Admin <admkiev@gmail.com>
2026-05-11 21:40:52 +0300

    edet user model

--- backend/app/apps/users/models.py
+++ backend/app/apps/users/models.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 from apps.core.base_models import Base
 
-#user
+#user assa
 class User(Base):
     name: Mapped[str]
     email: Mapped[str] = mapped_column(unique=True, index=True)
